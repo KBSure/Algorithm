8c95ae5e932c540108b7209d4cb880790e4d6d76
KBSure <kbsure.dev@gmail.com>
2018-04-04 01:53:18 +0900

    [Num1698]discovered[start] = true 를 빼먹어서 문제였다. 해결

--- src/Num1697.java
+++ src/Num1697.java
@@ -13,7 +13,7 @@ public class Num1697 {
 
     private static void init_adj(){
         for(int i = 0; i < NUM; i++){
-            if(i*2 < NUM ) {
+            if(i*2 < NUM && i*2 != i) { //이렇게 예외 처리를 하거나
                 adj[i][0] = i*2;
             }else{
                 adj[i][0] = -1;
@@ -34,7 +34,7 @@ public class Num1697 {
         //인접된 정점 기록한 int 2차배열
         //검사할 정점 리스트 queue<Integer>
         //정점까지의 distance 기록 int 배열
-        //
+
         init_adj();
 
         LinkedList<Integer> q = new LinkedList<>();
@@ -42,8 +42,11 @@ public class Num1697 {
         boolean discovered [] = new boolean[NUM];
         q.addLast(start);
         distance[start] = 0;
+        discovered[start] = true; //이렇게 초기화 작업 중요.
+        // 교훈 : 값을 Injection 하는 부분이나, 배열의 맨 앞이나 맨 뒤에서의 동작 등을 살펴보자.
 
-        while(q !=null){
+
+        while(!q.isEmpty()){
             int here = q.pollFirst();
 
             if(here == end){ //정답 검사
