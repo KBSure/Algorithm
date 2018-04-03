159bd1cd945efd790d3bc5adc7d276309c119c03
KBSure <kbsure.dev@gmail.com>
2018-04-03 21:32:40 +0900

    1697 시작

--- src/Num1181.java
+++ src/Num1181.java
@@ -12,31 +12,25 @@ public class Num1181 {
 
         for(int i = 0; i < wordsCount; i++){
             for(int j = 0; j < wordsCount - i - 1; j++){
-               if(words[j].length() > words[j+1].length()){
-                   String tmp = words[j];
-                   words[j] = words[j+1];
-                   words[j+1] = tmp;
-               }else if(words[j].length() == words[j+1].length()){
-                   //알파벳순
-                   for(int k = 0; k < words[j].length(); k++){
-                           if(words[j].charAt(k) == words[j+1].charAt(k)){
-                               continue;
-                           }else if(words[j].charAt(k) > words[j+1].charAt(k)){
-                               String tmp = words[j];
-                               words[j] = words[j+1];
-                               words[j+1] = tmp;
-                               break;
-                           }else if(words[j].charAt(k) < words[j+1].charAt(k)){
-                               break;
-                           }
-                   }
-               }
-            }
-        }
-
-        for(int i = 0; i < wordsCount; i++){
-            if(words[i].length() == words[i+1].length()){
-
+                if(words[j].length() > words[j+1].length()){
+                    String tmp = words[j];
+                    words[j] = words[j+1];
+                    words[j+1] = tmp;
+                }else if(words[j].length() == words[j+1].length()){
+                    //알파벳순
+                    for(int k = 0; k < words[j].length(); k++){
+                        if(words[j].charAt(k) == words[j+1].charAt(k)){
+                            continue;
+                        }else if(words[j].charAt(k) > words[j+1].charAt(k)){
+                            String tmp = words[j];
+                            words[j] = words[j+1];
+                            words[j+1] = tmp;
+                            break;
+                        }else if(words[j].charAt(k) < words[j+1].charAt(k)){
+                            break;
+                        }
+                    }
+                }
             }
         }
 
---
+++ src/Num1181_Merge.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Num1181_Merge {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int wordsCount = Integer.valueOf(scanner.nextLine());
+        String [] words = new String[wordsCount];
+
+        for(int i = 0; i < wordsCount; i++){
+            words[i] = scanner.nextLine();
+        }
+    //미완성
+    }
+}
