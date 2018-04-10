3559f1de83ebb0e9a54073014b7ec2402dca6d57
KBSure <kbsure.dev@gmail.com>
2018-04-11 00:05:01 +0900

    유기농 배추 초기화 셋팅

--- src/Num1012.java
+++ src/Num1012.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
 public class Num1012 {
+
+    private static int rowSize;
+    private static int colSize;
+    private static int cabageCount;
+    private static int map [][];
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final int caseCount = scanner.nextInt();
+
+        for(int i = 0; i < caseCount; i++){
+            rowSize = scanner.nextInt();
+            colSize = scanner.nextInt();
+            cabageCount = scanner.nextInt();
+            map = new int[colSize][rowSize];
+            for(int j = 0; j < cabageCount; j++){
+                map[scanner.nextInt()][scanner.nextInt()] = 1;
+            }
+            dfsAll();
+
+        }
+    }
+
+    private static void dfsAll(){
+        for(int i = 0; i < colSize; i++){
+            for(int j = 0; j < rowSize; j++){
+                if(visited[i][j] !=null)
+                    dfs(i, j);
+            }
+        }
+    }
+
+    private static void dfs(int startI, int startJ){
 
     }
 }
+
+
