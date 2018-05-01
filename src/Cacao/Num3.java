d5dcae48cec3f41b007d649c891ffd21fe12a35e
KBSure <kbsure.dev@gmail.com>
2018-05-02 03:10:25 +0900

    Num3 문제풀이 시작, Num3_GE 추가

--- src/Cacao/Num3.java
+++ src/Cacao/Num3.java
@@ -1,4 +1,20 @@
 package Cacao;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
 public class Num3 {
+    public static void main(String[] args) {
+        // 주입 받는다고 가정하고
+        
+        int cacheSize = 3;
+        String [] cities = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"};
+
+
+        LinkedList<Integer> lruQ = new LinkedList<>();
+
+
+    }
 }
