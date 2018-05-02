5ad64ceffbf433d375a845361063d7be79eccfc7
KBSure <kbsure.dev@gmail.com>
2018-05-02 12:04:04 +0900

    num3 완료

--- src/Cacao/Num3.java
+++ src/Cacao/Num3.java
@@ -1,20 +1,36 @@
 package Cacao;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.LinkedList;
 
 public class Num3 {
     public static void main(String[] args) {
-        // 주입 받는다고 가정하고
-        
-        int cacheSize = 3;
-        String [] cities = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"};
+        // 주입 받았다고 가정하고
+        int cacheSize = 0;
+        String [] cities = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA"};
+        int citiesSize = cities.length;
 
+        LinkedList<String> lruQ = new LinkedList<>();
+        int executeTime = 0;
 
-        LinkedList<Integer> lruQ = new LinkedList<>();
+        if (cacheSize == 0) {
+            executeTime = 5*citiesSize;
+        }else {
+            for (int i = 0; i < citiesSize; i++) {
+                if (lruQ.contains(cities[i])) {
+                    lruQ.remove(cities[i]);
+                    lruQ.addLast(cities[i]);
+                    executeTime += 1;
+                } else {
+                    if (lruQ.size() == cacheSize) { //cacheSize가 0일 때 예외 처리 필요
+                        lruQ.removeFirst();
+                    }
+                    lruQ.addLast(cities[i]);
+                    executeTime += 5;
+                }
 
-
+                System.out.println(lruQ);
+            }
+        }
+        System.out.println(executeTime);
     }
 }
