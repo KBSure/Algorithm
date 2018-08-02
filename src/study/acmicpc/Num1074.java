ecb21c975da32cf4db4951005f6fb35f024fe881
KBSure <kbsure.dev@gmail.com>
2018-08-03 01:51:25 +0900

    백준 1074번(Z) 해결

---
+++ src/study/acmicpc/Num1074.java
@@ -0,0 +1,35 @@
+package study.acmicpc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Num1074 {
+    private static int count = -1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        int N = Integer.parseInt(split[0]);
+        int r = Integer.parseInt(split[1]);
+        int c = Integer.parseInt(split[2]);
+
+        int size = (int)Math.pow(N, 2);
+
+        z(r, c, 0, 0, (int)Math.pow(2, N));
+    }
+
+    private static void z(int r, int c, int startI, int startJ, int size){
+        if(size == 1) {
+            count++;
+            if(startI == r && startJ == c) System.out.println(count);
+            return;
+        }
+
+        int half = size / 2;
+        z(r, c, startI, startJ, half);
+        z(r, c, startI, startJ+half, half);
+        z(r, c, startI+half, startJ, half);
+        z(r, c, startI+half, startJ+half, half);
+    }
+
+}
