8397e388c96afe5f23194c6cebcd11f9f8101ee6
KBSure <kbsure.dev@gmail.com>
2018-08-06 10:28:23 +0900

    백준 1463번(1로 만들기) 풀이2

--- src/study/acmicpc/Num1463.java
+++ src/study/acmicpc/Num1463.java
@@ -7,11 +7,13 @@ import java.util.Arrays;
 
 public class Num1463 {
     private static int[] cache;
+    private static boolean[] isCache;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         cache = new int[N+1];
-        Arrays.fill(cache, -1);
+        isCache = new boolean[N+1];
+//        Arrays.fill(cache, -1);
         System.out.println(make1(N));
     }
 
@@ -19,11 +21,13 @@ public class Num1463 {
         if(n == 1) return 0;
         //cache
         int a = Integer.MAX_VALUE, b = Integer.MAX_VALUE, c = Integer.MAX_VALUE ;
-        if(cache[n] != -1) return cache[n];
+//        if(cache[n] != -1) return cache[n];
+        if(isCache[n]) return cache[n];
         if(n % 3 == 0) a = make1(n / 3) + 1;
         if(n % 2 == 0) b = make1(n / 2) + 1;
         c = make1(n - 1) + 1;
         int ret = Math.min(Math.min(a,b),c);
+        isCache[n] = true;
         cache[n] = ret;
         return ret;
     }
