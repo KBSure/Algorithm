c8a763706c2bea56347e9ef5bab9bb6a4ae993c3
KBSure <kbsure.dev@gmail.com>
2018-11-17 20:39:29 +0900

    nhn 코드 수정(Leaf 1 일 경우 정답 예외 처리, Root에서 Cycle 형성되는 예외 처리)

--- src/nhn/real/main/Main.java
+++ src/nhn/real/main/Main.java
@@ -7,33 +7,48 @@ import nhn.real.main.util.AntNodes;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     private static int antRoomCount;
     private static Map<Integer, AntNode> antNodeMap;
     private static Answer answer;
+    private static AntNode root;
     private static List<AntNode> leafList;
+    private static int allDistance;
+
     public static void main(String[] args) throws IOException {
         if(!init()){ // cycle이면
             System.out.println(-1);
             return;
         }
 
-        //순회
-        for(int i = 1; i <= antNodeMap.size(); i++){
-            AntNode leaf = antNodeMap.get(i);
-            if(leaf.isLeaf()){
-                boolean[] visited = new boolean[antRoomCount+1];
-                AntNodes.traversal(antNodeMap, visited, answer, leaf, leaf, 0);
-            }
-        }
+        findAnswer();
 
         System.out.println(answer.getDistance());
         System.out.println(answer.getStartNum());
         System.out.println(answer.getEndNum());
+    }
+
+    private static void findAnswer(){
+        if(leafList.size() == 1){
+            AntNode leaf = leafList.get(0);
+            if(root.getNum() < leaf.getNum()){
+                answer.setStartNum(root.getNum());
+                answer.setEndNum(leaf.getNum());
+                answer.setDistance(allDistance);
+            }else{
+                answer.setStartNum(leaf.getNum());
+                answer.setEndNum(root.getNum());
+                answer.setDistance(allDistance);
+            }
+            return;
+        }
+        //순회
+        for(AntNode leaf : leafList){
+            boolean[] visited = new boolean[antRoomCount+1];
+            AntNodes.traversal(visited, answer, leaf, leaf, 0);
+        }
     }
 
     private static boolean init() throws IOException {
@@ -53,16 +68,28 @@ public class Main {
             AntNode parent = antNodeMap.get(parentNum);
             AntNode child = antNodeMap.get(childNum);
 
-            if(child.hasParent()){
+            if(child.hasParent()){ // parent가 이미 있는 child는 cycle
                 return false;
             }
+
             // 관계 맺어주기 (트리화)
             child.setParent(parent);
             child.setDistance(distance);
             parent.addChild(child);
-
-            // root 구해놓기 (아직)
         }
+
+        leafList = new ArrayList<>();
+        for(int i = 1; i <= antRoomCount; i++) {
+            AntNode node = antNodeMap.get(i);
+            if(node.isLeaf()){
+                leafList.add(node);
+            }
+            if(!node.hasParent()) root = node;
+            allDistance += node.getDistance();
+        }
+
+        //root가 없으면 cycle
+        if(root == null) return false;
 
         answer = new Answer(0, 0, 0);
         return true;
--- src/nhn/real/main/util/AntNodes.java
+++ src/nhn/real/main/util/AntNodes.java
@@ -3,10 +3,8 @@ package nhn.real.main.util;
 import nhn.real.main.structure.Answer;
 import nhn.real.main.structure.AntNode;
 
-import java.util.Map;
-
 public class AntNodes {
-    public static void traversal(Map<Integer, AntNode> antNodeMap, boolean[] visited, Answer answer, AntNode startNode, AntNode hereNode, int sum){
+    public static void traversal(boolean[] visited, Answer answer, AntNode startNode, AntNode hereNode, int sum){
         // 다른 leaf에 도착
         visited[hereNode.getNum()] = true;
         if(hereNode.isLeaf() && hereNode != startNode){
@@ -25,16 +23,28 @@ public class AntNodes {
             }
             return;
         }
+
         // parent로
         // root가 아니라면 부모쪽으로
         if(hereNode.hasParent() && !visited[hereNode.getParent().getNum()]) {
-            traversal(antNodeMap, visited, answer, startNode, hereNode.getParent(), sum + hereNode.getDistance());
+            traversal(visited, answer, startNode, hereNode.getParent(), sum + hereNode.getDistance());
         }
         // child로
         for(AntNode child : hereNode.getChildren()){
             if(!visited[child.getNum()]) {
-                traversal(antNodeMap, visited, answer, startNode, child, sum + child.getDistance());
+                traversal(visited, answer, startNode, child, sum + child.getDistance());
             }
         }
+    }
+
+    public static AntNode findRoot(int nodeCount, AntNode start){
+        boolean[] visited = new boolean[nodeCount+1];
+        AntNode root = start;
+        while(root.hasParent()){
+            root = root.getParent();
+            if(visited[root.getNum()]) return null;
+            visited[root.getNum()] = true;
+        }
+        return root;
     }
 }
