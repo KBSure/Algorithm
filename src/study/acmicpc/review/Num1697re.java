0455e8972a63505ec2c2e1bf89319aeeb3d1a65e
KBSure <kbsure.dev@gmail.com>
2019-01-22 16:14:42 +0900

    Num1697 숨바꼭질 (cache Type 변경, 속도 향상)

--- src/study/acmicpc/review/Num1697re.java
+++ src/study/acmicpc/review/Num1697re.java
@@ -3,38 +3,45 @@ package study.acmicpc.review;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Num1697re {
     private static int K;
     private static int N;
-    private static int[][] cache;
+    private static boolean[] cache;
     private static Queue<Node> queue;
 
     public static void main(String[] args) throws IOException {
         init();
         queue.add(new Node(K, 0));
+
         while(!queue.isEmpty()){
             Node here = queue.poll();
             int hNum = here.getNum();
             int hLevel = here.getLevel();
-            if(hNum == -1 || hNum > 200000) continue; // -1이거나 200000을 넘어가면 out
+            if(hNum < 0 || hNum > 100000) continue; // -1이거나 200000을 넘어가면 out
             if(N == hNum){ // 정답인지 비교
                 System.out.println(hLevel);
                 return;
             }
 
-            if(cache[hNum][0] == -2){ //cache에 있는지 cache에 없으면
-                int a = cache[hNum][0] = hNum + 1;
-                int b = cache[hNum][1] = hNum - 1;
-                int c = cache[hNum][2] = hNum * 2;
-                queue.add(new Node(a, hLevel+1));
-                queue.add(new Node(b, hLevel+1));
-                queue.add(new Node(c, hLevel+1));
+            if(!cache[hNum]){ //cache에 있는지 cache에 없으면
+                cache[hNum] = true;
+                queue.add(new Node(hNum+1, hLevel+1));
+                queue.add(new Node(hNum-1, hLevel+1));
+                queue.add(new Node(hNum*2, hLevel+1));
             }
         }
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        K = Integer.parseInt(split[0]);
+        N = Integer.parseInt(split[1]);
+        cache = new boolean[100001];
+        queue = new LinkedList<>();
     }
 
     private static class Node{
@@ -52,17 +59,5 @@ public class Num1697re {
         public int getLevel() {
             return level;
         }
-    }
-
-    private static void init() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] split = br.readLine().split(" ");
-        K = Integer.parseInt(split[0]);
-        N = Integer.parseInt(split[1]);
-        cache = new int[200001][3];
-        for(int[] c : cache){
-            Arrays.fill(c, -2);
-        }
-        queue = new LinkedList<>();
     }
 }
