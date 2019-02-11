e37929287e8d72a14bc8d8990b034a0b6050b0c6
KBSure <kbsure.dev@gmail.com>
2019-02-11 23:50:07 +0900

    TEST - (JVM 내부의 주소 여부)

---
+++ src/study/acmicpc/Test.java
@@ -0,0 +1,21 @@
+package study.acmicpc;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Test{
+    public static void main(String[] args) {
+        double d = 3.0f;
+        float f = (float)3.0;
+        float f2 = 3L;
+        double d2 = 3;
+        double pow = Math.pow(3, 4);
+        Test2 t2 = new Test2();
+        System.out.println(t2);
+        System.out.println(new int[3]);
+        System.out.println(new int[4]);
+        //콜스택의 주소?
+    }
+}
