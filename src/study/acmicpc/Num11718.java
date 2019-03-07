72945ac9582b5b7bb1cc70e4268c8da6ca84471a
KBSure <kbsure.dev@gmail.com>
2019-03-07 23:22:30 +0900

    Num11718 그대로 출력하기 (통과)

---
+++ src/study/acmicpc/Num11718.java
@@ -0,0 +1,16 @@
+package study.acmicpc; // 그대로 출력하기
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Num11718 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        while(line != null){
+            System.out.println(line);
+            line = br.readLine();
+        }
+    }
+}
