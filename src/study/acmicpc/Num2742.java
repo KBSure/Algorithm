6bfc438c92a7c9ecfcf79051ae0bae98043b64a7
KBSure <kbsure.dev@gmail.com>
2019-03-10 23:58:02 +0900

    Num2742 기찍N (통과)

---
+++ src/study/acmicpc/Num2742.java
@@ -0,0 +1,15 @@
+package study.acmicpc;
+
+import java.io.*;
+
+public class Num2742 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        for(int i = n; i > 0; i--){
+            bw.write(i + "\n");
+        }
+        bw.close();
+    }
+}
