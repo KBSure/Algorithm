88b78754bd8069ea57799bb1fb5040633a0965a5
KBSure <kbsure.dev@gmail.com>
2019-03-17 23:54:54 +0900

    190316 코딩테스트 Num1

---
+++ src/nhn/realtest/Num1.java
@@ -0,0 +1,21 @@
+package nhn.realtest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Num1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int mark = (int)Math.sqrt((double)n);
+        for(int i = mark; i >= 1; i--){
+            if(n % i == 0){
+                int w = i;
+                int h = n / w;
+                System.out.println(h - w);
+                break;
+            }
+        }
+    }
+}
