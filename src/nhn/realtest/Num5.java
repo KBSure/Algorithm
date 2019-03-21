c4877ea52962c6dcbfc88f992b322fd910a826c6
KBSure <kbsure.dev@gmail.com>
2019-03-21 19:02:03 +0900

    Line 코딩테스트 Num5 풀이

---
+++ src/nhn/realtest/Num5.java
@@ -0,0 +1,63 @@
+package nhn.realtest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Num5 {
+    private static int c;
+    private static int b;
+    private static Queue<Node> queue;
+    private static boolean[] cache;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        c = Integer.parseInt(split[0]);
+        b = Integer.parseInt(split[1]);
+        queue = new LinkedList<>();
+        cache = new boolean[200_000];
+
+        int hereC = c;
+        queue.add(new Node(b,0));
+
+        while(!queue.isEmpty()){
+            Node hereB = queue.poll();
+            int hBNum = hereB.num;
+            int hBLevel = hereB.level;
+
+            hereC = c + hereB.level*(hereB.level+1)/2;
+
+            if(hereC > 200_000){
+                System.out.println(-1);
+                return;
+            }
+
+            if(hBNum < 0 || hBNum > 200_000) continue;
+
+            if(hereC == hBNum){ // 정답인지 비교
+                System.out.println(hBLevel);
+                return;
+            }
+
+            if(!cache[hBNum]){ //cache에 있는지 cache에 없으면
+                cache[hBNum] = true;
+                queue.add(new Node(hBNum+1, hBLevel+1));
+                queue.add(new Node(hBNum-1, hBLevel+1));
+                queue.add(new Node(hBNum*2, hBLevel+1));
+            }
+        }
+
+    }
+
+    static class Node {
+        int num;
+        int level;
+
+        Node(int num, int level) {
+            this.num = num;
+            this.level = level;
+        }
+    }
+}
